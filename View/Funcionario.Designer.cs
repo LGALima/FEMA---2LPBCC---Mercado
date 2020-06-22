7e2fa13bc4395e335244f697e094aa4ce8f05818
peresthayna <thaynapamela27@gmail.com>
2020-06-22 19:41:43 -0300

    Criação do formulário Funcionario

---
+++ View/Funcionario.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Mercado_Thay_and_Lusca
+{
+    partial class Funcionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblAdm = new System.Windows.Forms.Label();
+            this.lblImagem = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.rdbAdmS = new System.Windows.Forms.RadioButton();
+            this.rdbAdmN = new System.Windows.Forms.RadioButton();
+            this.imagemFuncionarioPb = new System.Windows.Forms.PictureBox();
+            this.imagemFuncionarioBtn = new System.Windows.Forms.Button();
+            this.imagemFuncionario = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.imagemFuncionarioPb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(81, 142);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(87, 24);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "Usuário:";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(92, 190);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(76, 24);
+            this.lblSenha.TabIndex = 1;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(96, 238);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(72, 24);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblAdm
+            // 
+            this.lblAdm.AutoSize = true;
+            this.lblAdm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdm.Location = new System.Drawing.Point(23, 286);
+            this.lblAdm.Name = "lblAdm";
+            this.lblAdm.Size = new System.Drawing.Size(145, 24);
+            this.lblAdm.TabIndex = 3;
+            this.lblAdm.Text = "Administrador:";
+            // 
+            // lblImagem
+            // 
+            this.lblImagem.AutoSize = true;
+            this.lblImagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImagem.Location = new System.Drawing.Point(72, 334);
+            this.lblImagem.Name = "lblImagem";
+            this.lblImagem.Size = new System.Drawing.Size(96, 24);
+            this.lblImagem.TabIndex = 4;
+            this.lblImagem.Text = "Imagem: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(274, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(283, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Cadastro de Funcionários";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(202, 142);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(201, 20);
+            this.txtUser.TabIndex = 6;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(202, 193);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(201, 20);
+            this.txtSenha.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(202, 241);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(201, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // rdbAdmS
+            // 
+            this.rdbAdmS.AutoSize = true;
+            this.rdbAdmS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbAdmS.Location = new System.Drawing.Point(229, 290);
+            this.rdbAdmS.Name = "rdbAdmS";
+            this.rdbAdmS.Size = new System.Drawing.Size(49, 20);
+            this.rdbAdmS.TabIndex = 9;
+            this.rdbAdmS.TabStop = true;
+            this.rdbAdmS.Text = "Sim";
+            this.rdbAdmS.UseVisualStyleBackColor = true;
+            // 
+            // rdbAdmN
+            // 
+            this.rdbAdmN.AutoSize = true;
+            this.rdbAdmN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbAdmN.Location = new System.Drawing.Point(329, 290);
+            this.rdbAdmN.Name = "rdbAdmN";
+            this.rdbAdmN.Size = new System.Drawing.Size(52, 20);
+            this.rdbAdmN.TabIndex = 10;
+            this.rdbAdmN.TabStop = true;
+            this.rdbAdmN.Text = "Não";
+            this.rdbAdmN.UseVisualStyleBackColor = true;
+            // 
+            // imagemFuncionarioPb
+            // 
+            this.imagemFuncionarioPb.Location = new System.Drawing.Point(495, 125);
+            this.imagemFuncionarioPb.Name = "imagemFuncionarioPb";
+            this.imagemFuncionarioPb.Size = new System.Drawing.Size(250, 250);
+            this.imagemFuncionarioPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagemFuncionarioPb.TabIndex = 11;
+            this.imagemFuncionarioPb.TabStop = false;
+            // 
+            // imagemFuncionarioBtn
+            // 
+            this.imagemFuncionarioBtn.Location = new System.Drawing.Point(202, 334);
+            this.imagemFuncionarioBtn.Name = "imagemFuncionarioBtn";
+            this.imagemFuncionarioBtn.Size = new System.Drawing.Size(98, 23);
+            this.imagemFuncionarioBtn.TabIndex = 12;
+            this.imagemFuncionarioBtn.Text = "Selecione";
+            this.imagemFuncionarioBtn.UseVisualStyleBackColor = true;
+            this.imagemFuncionarioBtn.Click += new System.EventHandler(this.imagemFuncionarioBtn_Click);
+            // 
+            // imagemFuncionario
+            // 
+            this.imagemFuncionario.FileName = "imagemFuncionario";
+            // 
+            // Funcionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imagemFuncionarioBtn);
+            this.Controls.Add(this.imagemFuncionarioPb);
+            this.Controls.Add(this.rdbAdmN);
+            this.Controls.Add(this.rdbAdmS);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblImagem);
+            this.Controls.Add(this.lblAdm);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Funcionario";
+            this.Text = "Funcionario";
+            ((System.ComponentModel.ISupportInitialize)(this.imagemFuncionarioPb)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblAdm;
+        private System.Windows.Forms.Label lblImagem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.RadioButton rdbAdmS;
+        private System.Windows.Forms.RadioButton rdbAdmN;
+        private System.Windows.Forms.PictureBox imagemFuncionarioPb;
+        private System.Windows.Forms.Button imagemFuncionarioBtn;
+        private System.Windows.Forms.OpenFileDialog imagemFuncionario;
+    }
+}
---
+++ View/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mercado_Thay_and_Lusca
+{
+    public partial class Funcionario : Form
+    {
+        public Funcionario()
+        {
+            InitializeComponent();
+        }
+
+        private void imagemFuncionarioBtn_Click(object sender, EventArgs e)
+        {
+            imagemFuncionario.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if(imagemFuncionario.ShowDialog()!=DialogResult.Cancel)
+            {
+                imagemFuncionarioPb.Image = new Bitmap(imagemFuncionario.FileName);
+                imagemFuncionarioBtn.Text = imagemFuncionario.SafeFileName;
+            }
+        }
+    }
+}
